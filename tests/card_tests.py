30f1fb74e56c94c156698dfb51f94ce35ad6c80d
yordan-marinov <jordanmarinov8@gmail.com>
2021-04-01 14:48:13 +0300

    Updated file: 'card_tests'

--- tests/card_tests.py
+++ tests/card_tests.py
@@ -12,7 +12,7 @@ class CardTests(TestCase):
     def test_card_correct_init(self):
         self.assertEqual("6E536046010080FF", self.c1.card_id)
         self.assertEqual("445E6046010080FF", self.c2.card_id)
-        
+
     def test_get_expired_date_is_returns_correct_timestamp_as_int(self):
         now = time.time()
         exparied_date = time.time() + 1
