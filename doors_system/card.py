145e4d85d4c6d209285c12dce466df614a343acf
yordan-marinov <jordanmarinov8@gmail.com>
2021-04-01 09:11:58 +0300

    Added doc strings for better descriptions

--- doors_system/card.py
+++ doors_system/card.py
@@ -5,16 +5,20 @@ class Card:
     __card_valid_period_days = 30
     # This shoud be 30 days but for testing is set to seconds
 
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, card_id):
+        """Inicialization of card with ID and Expired date"""
+
+        self.card_id = card_id
         self.expired_date = self.get_expired_date()
 
     @staticmethod
     def get_expired_date(period_days=__card_valid_period_days):
+        """Sets Expired date property, by default is 30 days"""
+
         end_date = datetime.now() + timedelta(seconds=period_days)
         # Should be days not seconds in timedelta paramerters
         end_timestamp = datetime.timestamp(end_date)
         return int(end_timestamp)
 
     def __repr__(self):
-        return f"card id: {self.id} with expired date: {self.expired_date}"
+        return f"Card id: ({self.card_id}) expired date: ({self.expired_date})"
