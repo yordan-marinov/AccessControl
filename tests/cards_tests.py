a01517258c9c46bcc9bfba6b00152e2b33dab932
yordan-marinov <jordanmarinov8@gmail.com>
2021-04-02 11:02:53 +0300

    Updated file: 'cards_tests'

--- tests/cards_tests.py
+++ tests/cards_tests.py
@@ -2,6 +2,7 @@ from doors_system.card import Card
 from doors_system.cards import Cards
 
 from unittest import TestCase, main
+import time
 
 
 class TestsCards(TestCase):
@@ -15,7 +16,29 @@ class TestsCards(TestCase):
 
     def test_add_card(self):
         self.cards.add_card(self.c1)
+        self.cards.add_card(self.c2)
+        self.assertIn(self.c1, self.cards.all_cards)
+        self.assertIn(self.c2, self.cards.all_cards)
+
+    def test_add_same_card_twice(self):
+        self.cards.add_card(self.c1)
+        self.cards.add_card(self.c1)
         self.assertEqual(1, len(self.cards.all_cards))
+
+    def test_remove_card_if_in_bd(self):
+        self.cards.add_card(self.c1)
+        self.cards.remove_card(self.c1)
+        self.assertNotIn(self.c1, self.cards.all_cards)
+
+    def test_remove_card_if_not_in_bd(self):
+        self.cards.remove_card(self.c1)
+        self.assertNotIn(self.c1, self.cards.all_cards)
+
+    def test_find_card_by_id(self):
+        self.c1 = Card("6E536046010080FF")
+        self.cards.add_card(self.c1)
+        expected = self.cards.find_card_by_id(self.c1.card_id)
+        self.assertEqual(self.c1, expected)
 
 
 if __name__ == "__main__":
